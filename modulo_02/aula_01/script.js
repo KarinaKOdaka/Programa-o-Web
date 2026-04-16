40f5d4cbf7fe0c22319896d0844a0122a6351eb9
Karina <karinakodaka@gmail.com>
2026-04-16 20:33:30 -0300

    Atualizando Atividade final

--- modulo_02/aula_01/script.js
+++ modulo_02/aula_01/script.js
@@ -1,1 +1,18 @@
-var nome = 'AAA';
+/*let nome = 'AAA';
+nome = "A";*/
+
+function olanome(){
+    console.log(nome)
+}
+
+function setValorNome(){
+    nome = "Ana"
+}
+
+function main(){
+    setValorNome()
+    olanome()
+}
+
+console.log(nome);
+console.log(2 + 2); 
